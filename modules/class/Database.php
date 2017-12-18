d232b92a9614974d85c02d401830b636a02f63e9
Koki Yamamoto <yamakoh@KokinoMacBook-Pro.local>
2017-12-19 03:36:08 +0900

    insert機能を追加

--- modules/class/Database.php
+++ modules/class/Database.php
@@ -10,8 +10,8 @@ class Database{
     protected $dsn = 'mysql:host=localhost;port=3306;dbname=sd22_master';//左から,ホスト名,ポート番号,DB名
     protected $user = 'root';// ユーザー名
     protected $pass = 'root';//パスワード
-    protected $dbh; //DBハンドラ
-    protected $stmt;
+    public $dbh; //DBハンドラ
+    public $stmt; //DBステートメント
 
     //コンストラクタ
     // public function __construct(){ 
@@ -25,24 +25,26 @@ class Database{
         try{
             $this->dbh = new PDO(
                 $this->dsn, $this->$user, $this->$pass,
-                array( PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`" )
+                array( PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`",
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false)     
             );       
 
             //トランザクションを初期化する
             $this->dbh->beginTransaction();
-            echo 'ok';
 
         }catch (PDOException $e){
             print('Connection failed:'.$e->getMessage());
             die();
         }
+        
         //display errors
         $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         return $this->dbh;
     }
 
     /**
-     * PDOの後処理用クラス
+     * PDOの後処理
      */
     private function closePDO(){
         $this->dbh = null;
@@ -55,16 +57,53 @@ class Database{
      */
     public function select($query){
         $pdo = $this->openPDO();
-        $this->stmt = $pdo->query($query);
-        $result = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $pdo->query($query);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $this->closePDO();
         return $result;
     }
 
-    // public function insert($arrQuery){
-    //     $pdo = $this->openPDO();
-    //     $this->stmt = $pdo->prepare($arrQuery);
-    // }
+    /**
+     * 入力されたsqlを元にselect文を実行
+     * @param string $table 入力されたsql文
+     * @param array $arrColumns 入力されたsql文
+     * @param array $arrParams 入力されたsql文
+     */  
+    public function insert($table,$arrColumns,$arrParams){
+        if(count($arrColumns) != count($arrParams)){
+            return 'error:array count no match';
+            die();
+        }
+        $pdo = $this->openPDO();
+        $cnt = count($arrColumns);
+        $limit = $cnt-1;
+        $holder = '';
+        $insertParams = array();
+        
+        $query = 'insert into '.$table.' (';
+        for($i = 0;$i < $cnt;$i++){
+            if($i != $limit){
+                $query .= $arrColumns[$i].',';
+                $holder .= ':'.$arrColumns[$i].',';
+                //$arrBinders[] = ':'.$arrColumns[$i];
+                $insertParams = $insertParams + array(':'.$arrColumns[$i] => $arrParams[$i]);
+     
+            }else{
+                $query .= $arrColumns[$i];     
+                $holder .= ':'.$arrColumns[$i];
+                //$arrBinders[] = ':'.$arrColumns[$i];
+                $insertParams = $insertParams + array(':'.$arrColumns[$i] => $arrParams[$i]);
+            }     
+        }
+      
+        $query .= ') values ('.$holder.') ;';
+        var_dump($query);
+        var_dump($insertParams);
+        
+        $stmt = $pdo->prepare($query);
+        $stmt->execute($insertParams);
+        $this->closePDO();
+    }
 
     // public function delete(){
 
@@ -74,3 +113,4 @@ class Database{
 
     // }
 } 
+?>
--- readme.txt
+++ readme.txt
@@ -8,6 +8,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/modules/class/Database.php';
 ファイルの読み込みなので、可能な限りソースコードの一番上の方に追記するのが望ましいです。
 
 2.使用方法（例）
+select()
 $var = new Database(); //インスタンスの生成
 $sql = 'select * from members'; //sql文
 $result -> $var->select($sql); //select文を実行、$resultに結果を連想配列で格納
@@ -22,6 +23,18 @@ foreach($result as $key=>$val){
 }
 
 ※insert,updateについては追ってコミットします。
+
+insert()
+1.$var = new Database(); //インスタンスの生成
+
+2.インサートしたいカラム名、カラムに入れたい値を配列に入力
+$tableName = 'tblName';
+$insertColumns = array('col1','col2');
+$insertValues = array('10','aaa');
+
+3.$var->insert($tableName,$insertColumns,$insertValues);
+第1引数にテーブル名、第2引数にカラム名を入れた配列、第3引数に追加したい値入れた配列
+これらを入力し実行
 
 
 
